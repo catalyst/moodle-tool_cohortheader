16dc21beb4641b24b0dd763474bab6ff54da369d
Antony Videtta <antonyvidetta@catalyst.net.nz>
2021-07-23 09:00:24 +1200

    Swap release version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_cohortheader';
-$plugin->release = '0.2.0';
-$plugin->version = 2021070401;
+$plugin->release = '0.3.0';
+$plugin->version = 2021070403;
 $plugin->requires = 2021010800;
 $plugin->maturity = MATURITY_STABLE;
